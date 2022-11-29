c94a667bd2025b98131c1a7a156a2e4abba89705
Phydon <84594612+Phydon@users.noreply.github.com>
2022-11-29 22:23:19 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@ use std::{
     error::Error,
     process::{self, Command},
     sync::Arc,
-    time::Instant,
+    thread,
+    time::{Duration, Instant},
 };
 
 fn main() {
@@ -25,11 +26,16 @@ fn main() {
         .unwrap();
 
     let commands = vec![
-        "echo \"TEST1\";Start-sleep -Seconds(1)".to_string(),
-        "echo \"TEST2\";Start-sleep -Seconds(1)".to_string(),
-        "echo \"TEST3\";Start-sleep -Seconds(1)".to_string(),
-        "echo \"TEST4\";Start-sleep -Seconds(1)".to_string(),
-        "echo \"TEST5\";Start-sleep -Seconds(1)".to_string(),
+        // "echo \"START1\";Start-sleep -Seconds(2);echo \"END1\"".to_string(),
+        // "echo \"START2\";Start-sleep -Seconds(3);echo \"END2\"".to_string(),
+        // "echo \"START3\";Start-sleep -Seconds(2);echo \"END3\"".to_string(),
+        // "echo \"START4\";Start-sleep -Seconds(1);echo \"END4\"".to_string(),
+        // "echo \"START5\";Start-sleep -Seconds(4);echo \"END5\"".to_string(),
+        "Start-sleep -seconds 4".to_string(),
+        "Start-sleep -seconds 2".to_string(),
+        "Start-sleep -seconds 2".to_string(),
+        "Start-sleep -seconds 1".to_string(),
+        "Start-sleep -seconds 3".to_string(),
         // "scoop update".to_string(),
         // "scoop status".to_string(),
         // "winget upgrade".to_string(),
@@ -85,35 +91,91 @@ fn cmd(args: &str) -> Result<(), Box<dyn Error>> {
 //     Ok(combiner)
 // }
 
+// fn progress_bar(commands: Vec<String>, num: u64) -> Result<Arc<MultiProgress>, Box<dyn Error>> {
+//     let started = Instant::now();
+
+//     let m = Arc::new(MultiProgress::new());
+//     let sty = ProgressStyle::with_template(
+//         "{spinner:.green} [{elapsed_precise}] {bar:40.cyan/blue} {pos:>5}/{len:5} {eta:5} {msg}",
+//     )
+//     .unwrap()
+//     .progress_chars("#>-");
+
+//     let pb = m.add(ProgressBar::new(num));
+//     pb.set_style(sty.clone());
+
+//     pb.tick();
+//     for arg in commands {
+//         cmd(arg.as_str())?;
+
+//         // let pb2 = m.add(ProgressBar::new(128));
+//         // pb2.set_style(sty.clone());
+//         // for _ in 0..128 {
+//         //     pb2.inc(1);
+//         //     thread::sleep(Duration::from_millis(5));
+//         // }
+//         // pb2.finish();
+
+//         pb.inc(1);
+//     }
+//     pb.finish_with_message(format!("{}", "done".bold().green()));
+
+//     println!(
+//         "{} {}",
+//         "::: DONE IN".bold().green(),
+//         HumanDuration(started.elapsed()).to_string().bold().green()
+//     );
+
+//     Ok(m)
+// }
+
 fn progress_bar(commands: Vec<String>, num: u64) -> Result<Arc<MultiProgress>, Box<dyn Error>> {
     let started = Instant::now();
+    let spinner_style = ProgressStyle::with_template("{prefix:.bold.dim} {spinner} {wide_msg}")
+        .unwrap()
+        .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈ ");
 
     let m = Arc::new(MultiProgress::new());
     let sty = ProgressStyle::with_template(
         "{spinner:.green} [{elapsed_precise}] {bar:40.cyan/blue} {pos:>5}/{len:5} {eta:5} {msg}",
     )
     .unwrap()
-    // .progress_chars("#>-");
-    .progress_chars("##-");
+    .progress_chars("#>-");
 
     let pb = m.add(ProgressBar::new(num));
+    // TODO remove clone?
     pb.set_style(sty.clone());
 
     pb.tick();
-    for arg in commands {
-        cmd(arg.as_str())?;
+    // let handles: Vec<_> = (0..num)
+    let handles: Vec<_> = commands
+        .into_iter()
+        .map(|arg| {
+            // // FIXME
+            let pb = ProgressBar::new_spinner();
+            pb.enable_steady_tick(Duration::from_millis(200));
+            pb.set_style(spinner_style.clone());
+            // let pb = m.add(ProgressBar::new(num));
+            // pb.set_style(spinner_style.clone());
+            pb.set_prefix(format!("[..]"));
+            thread::spawn(move || {
+                pb.set_message(format!("{}", "updating".red()));
+                // pb.inc(1);
+                pb.tick();
+                cmd(arg.as_str()).unwrap();
+                pb.finish_with_message(format!("{}", "done".bold().green()));
+                // pb.finish();
+            })
+        })
+        .collect();
 
-        // let pb2 = m.add(ProgressBar::new(128));
-        // pb2.set_style(sty.clone());
-        // for _ in 0..128 {
-        //     pb2.inc(1);
-        //     thread::sleep(Duration::from_millis(5));
-        // }
-        // pb2.finish();
-
-        pb.inc(1);
+    for h in handles {
+        let _ = h.join();
     }
+
     pb.finish_with_message(format!("{}", "done".bold().green()));
+
+    m.clear().unwrap();
 
     println!(
         "{} {}",
