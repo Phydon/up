5abe054406d1b43577b9e660c526865d96cb4385
Phydon <84594612+Phydon@users.noreply.github.com>
2022-11-29 20:01:30 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@
 
 use colored::*;
 use flexi_logger::{detailed_format, Duplicate, FileSpec, Logger};
+use indicatif::{HumanDuration, MultiProgress, ProgressBar, ProgressStyle};
 use log::error;
 
 use std::{
     error::Error,
     process::{self, Command},
+    sync::Arc,
+    time::Instant,
 };
 
 fn main() {
@@ -22,21 +25,18 @@ fn main() {
         .unwrap();
 
     let commands = vec![
-        format!("echo \"{}\"", "::: STARTING UPDATE".bold().yellow()),
-        format!("echo \"{}\"", "::: updating scoop ...".cyan()),
-        "scoop update".to_string(),
-        "scoop status".to_string(),
-        format!("echo \"{}\"", "::: updating winget ...".cyan()),
-        "winget upgrade".to_string(),
-        format!("echo \"{}\"", "::: updating rust ...".cyan()),
-        "rustup --verbose update".to_string(),
-        format!("echo \"{}\"", "::: updating vim ...".cyan()),
-        "vim -c PlugUpdate -c qa".to_string(),
-        format!("echo \"{}\"", "::: updating nvim ...".cyan()),
-        "nvim -c PlugUpdate -c qa".to_string(),
-        format!("echo \"{}\"", "::: updating haskell ...".cyan()),
-        "ghcup --verbose upgrade".to_string(),
-        format!("echo \"{}\"", "::: DONE".bold().green()),
+        "echo \"TEST1\";Start-sleep -Seconds(1)".to_string(),
+        "echo \"TEST2\";Start-sleep -Seconds(1)".to_string(),
+        "echo \"TEST3\";Start-sleep -Seconds(1)".to_string(),
+        "echo \"TEST4\";Start-sleep -Seconds(1)".to_string(),
+        "echo \"TEST5\";Start-sleep -Seconds(1)".to_string(),
+        // "scoop update".to_string(),
+        // "scoop status".to_string(),
+        // "winget upgrade".to_string(),
+        // "rustup update".to_string(),
+        // "vim -c PlugUpdate -c qa".to_string(),
+        // "nvim -c PlugUpdate -c qa".to_string(),
+        // "ghcup upgrade".to_string(),
         // "Start-sleep -seconds 5".to_string(),
     ];
 
@@ -46,9 +46,18 @@ fn main() {
     }
 }
 
+// fn run_cmd(commands: Vec<String>) -> Result<(), Box<dyn Error>> {
+//     let args: String = collect_args(commands)?;
+//     cmd(args.as_str())?;
+
+//     Ok(())
+// }
+
 fn run_cmd(commands: Vec<String>) -> Result<(), Box<dyn Error>> {
-    let args: String = collect_args(commands)?;
-    cmd(args.as_str())?;
+    println!("{}", "::: STARTING UPDATE".bold().yellow());
+
+    let num = commands.len() as u64;
+    progress_bar(commands, num)?;
 
     Ok(())
 }
@@ -66,12 +75,51 @@ fn cmd(args: &str) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn collect_args(args_list: Vec<String>) -> Result<String, Box<dyn Error>> {
-    let mut combiner = String::new();
-    for arg in args_list {
-        combiner.push_str(&arg);
-        combiner.push_str("; ");
-    }
+// fn collect_args(args_list: Vec<String>) -> Result<String, Box<dyn Error>> {
+//     let mut combiner = String::new();
+//     for arg in args_list {
+//         combiner.push_str(&arg);
+//         combiner.push_str("; ");
+//     }
 
-    Ok(combiner)
+//     Ok(combiner)
+// }
+
+fn progress_bar(commands: Vec<String>, num: u64) -> Result<Arc<MultiProgress>, Box<dyn Error>> {
+    let started = Instant::now();
+
+    let m = Arc::new(MultiProgress::new());
+    let sty = ProgressStyle::with_template(
+        "{spinner:.green} [{elapsed_precise}] {bar:40.cyan/blue} {pos:>5}/{len:5} {eta:5} {msg}",
+    )
+    .unwrap()
+    // .progress_chars("#>-");
+    .progress_chars("##-");
+
+    let pb = m.add(ProgressBar::new(num));
+    pb.set_style(sty.clone());
+
+    pb.tick();
+    for arg in commands {
+        cmd(arg.as_str())?;
+
+        // let pb2 = m.add(ProgressBar::new(128));
+        // pb2.set_style(sty.clone());
+        // for _ in 0..128 {
+        //     pb2.inc(1);
+        //     thread::sleep(Duration::from_millis(5));
+        // }
+        // pb2.finish();
+
+        pb.inc(1);
+    }
+    pb.finish_with_message(format!("{}", "done".bold().green()));
+
+    println!(
+        "{} {}",
+        "::: DONE IN".bold().green(),
+        HumanDuration(started.elapsed()).to_string().bold().green()
+    );
+
+    Ok(m)
 }
