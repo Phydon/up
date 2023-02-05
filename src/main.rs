114cc3edd61f53dbf178370347c7b9548d18248e
Phydon <84594612+Phydon@users.noreply.github.com>
2023-02-05 16:19:38 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use indicatif::{HumanDuration, MultiProgress, ProgressBar, ProgressStyle};
 use log::error;
 
 use std::{
+    env,
     error::Error,
     process::{self, Command},
     sync::Arc,
@@ -11,26 +12,74 @@ use std::{
     time::{Duration, Instant},
 };
 
+// redish bg, red fg
+const F1: u8 = 255;
+const F2: u8 = 46;
+const F3: u8 = 95;
+const B1: u8 = 41;
+const B2: u8 = 0;
+const B3: u8 = 25;
+
+// green
+const F4: u8 = 10;
+const F5: u8 = 255;
+const F6: u8 = 169;
+
+// purple
+const F7: u8 = 127;
+const F8: u8 = 83;
+const F9: u8 = 191;
+
 struct Program {
     name: String,
     start_extern: bool,
+    output: bool,
+    outputfile: String,
     collected_cmds: String,
 }
 
 impl Program {
-    pub fn new(name: &str, executer: &str, start_extern: bool, cmds: Vec<&str>) -> Program {
+    pub fn new(
+        name: &str,
+        executer: &str,
+        start_extern: bool,
+        output: bool,
+        cmds: Vec<&str>,
+    ) -> Program {
+        let tmp_dir = env::temp_dir();
+        let tmp = tmp_dir.to_string_lossy() + "up_output_" + name + ".txt";
+        let outputfile = tmp.to_string();
+
         let mut collected_cmds = String::new();
         match start_extern {
-            true => {
-                for cmd in cmds {
-                    collected_cmds.push_str("Start-Process ");
-                    collected_cmds.push_str(executer);
-                    collected_cmds.push_str(" -ArgumentList '");
-                    collected_cmds.push_str(cmd);
-                    collected_cmds.push_str("'");
-                    collected_cmds.push_str(";");
+            true => match output {
+                true => {
+                    for cmd in cmds {
+                        collected_cmds.push_str("Start-Process ");
+                        collected_cmds.push_str(executer);
+                        collected_cmds.push_str(" -ArgumentList '");
+                        collected_cmds.push_str(cmd);
+                        collected_cmds.push_str("'");
+                        collected_cmds.push_str("-RedirectStandardOutput ");
+                        collected_cmds.push_str(outputfile.as_str());
+                        collected_cmds.push_str(" -WindowStyle Hidden");
+                        collected_cmds.push_str(" -Wait");
+                        collected_cmds.push_str(";");
+                    }
                 }
-            }
+                false => {
+                    for cmd in cmds {
+                        collected_cmds.push_str("Start-Process ");
+                        collected_cmds.push_str(executer);
+                        collected_cmds.push_str(" -ArgumentList '");
+                        collected_cmds.push_str(cmd);
+                        collected_cmds.push_str("'");
+                        collected_cmds.push_str(" -WindowStyle Hidden");
+                        collected_cmds.push_str(" -Wait");
+                        collected_cmds.push_str(";");
+                    }
+                }
+            },
             false => {
                 for cmd in cmds {
                     collected_cmds.push_str(executer);
@@ -46,6 +95,8 @@ impl Program {
         Program {
             name,
             start_extern,
+            output,
+            outputfile,
             collected_cmds,
         }
     }
@@ -63,13 +114,25 @@ fn main() {
         .unwrap();
 
     // set up the programs
-    let scoop = Program::new("scoop", "scoop", true, vec!["update --all"]);
-    let winget = Program::new("winget", "winget", true, vec!["upgrade"]);
-    let rust = Program::new("rust", "rustup", true, vec!["update"]);
-    let haskell = Program::new("haskel", "ghcup", true, vec!["update"]);
-    let vim = Program::new("vim", "vim", true, vec!["-c PlugUpdate -c qa"]);
-    let nvim = Program::new("nvim", "nvim", true, vec!["-c PlugUpdate -c qa"]);
-    let pip = Program::new("pip", "py", true, vec!["-m pip install --upgrade pip"]);
+    let scoop = Program::new(
+        "scoop",
+        "powershell",
+        true,
+        true,
+        vec!["-c scoop update --all"],
+    );
+    let winget = Program::new("winget", "winget", true, true, vec!["upgrade"]);
+    let rust = Program::new("rust", "rustup", true, true, vec!["update"]);
+    let haskell = Program::new("haskel", "ghcup", true, true, vec!["update"]);
+    let vim = Program::new("vim", "vim", true, false, vec!["-c PlugUpdate -c qa"]);
+    let nvim = Program::new("nvim", "nvim", true, false, vec!["-c PlugUpdate -c qa"]);
+    let pip = Program::new(
+        "pip",
+        "py",
+        true,
+        true,
+        vec!["-m pip install --upgrade pip"],
+    );
 
     let commands: Vec<Program> = vec![scoop, winget, rust, haskell, vim, nvim, pip];
 
@@ -86,7 +149,8 @@ fn main() {
 }
 
 fn update(commands: Vec<Program>) -> Result<(), Box<dyn Error>> {
-    println!("{}", "::: STARTING UPDATE".bold().yellow());
+    // println!("{}", "::: STARTING UPDATE".bold().yellow());
+    println!("{}", "::: STARTING UPDATE".bold().truecolor(F7, F8, F9));
 
     let num = commands.len() as u64;
     progress_bar(commands, num)?;
@@ -115,7 +179,7 @@ fn progress_bar(commands: Vec<Program>, num: u64) -> Result<Arc<MultiProgress>, 
 
     let m = Arc::new(MultiProgress::new());
     let sty = ProgressStyle::with_template(
-        "{spinner:.green} [{elapsed_precise}] {bar:40.yellow/red} {pos:>5}/{len:5} {eta:5} {msg}",
+        "{spinner:.blue} [{elapsed_precise}] {bar:40.red/blue} {pos:>5}/{len:5} {eta:5} {msg}",
     )
     .unwrap()
     // .progress_chars("#>-");
@@ -134,15 +198,33 @@ fn progress_bar(commands: Vec<Program>, num: u64) -> Result<Arc<MultiProgress>, 
             spinner.set_style(spinner_style.clone());
             spinner.set_prefix(format!("[..]"));
             thread::spawn(move || {
-                spinner.set_message(format!("{} {}", "updating".yellow(), arg.name));
+                spinner.set_message(format!("{} {}", "updating".truecolor(F7, F8, F9), arg.name));
                 spinner.tick();
                 run_cmd(arg.collected_cmds.as_str()).unwrap();
                 spinner.finish_with_message(match arg.start_extern {
-                    true => {
-                        format!("{} {}", arg.name, "started".green())
-                    }
+                    true => match arg.output {
+                        true => {
+                            format!(
+                                "{} {}  => output at \"{}\"",
+                                arg.name.truecolor(F7, F8, F9),
+                                "done".truecolor(F4, F5, F6),
+                                arg.outputfile.italic(),
+                            )
+                        }
+                        false => {
+                            format!(
+                                "{} {}",
+                                arg.name.truecolor(F7, F8, F9),
+                                "done".truecolor(F4, F5, F6)
+                            )
+                        }
+                    },
                     false => {
-                        format!("{} {}", arg.name, "done".bold().green())
+                        format!(
+                            "{} {}",
+                            arg.name.truecolor(F7, F8, F9),
+                            "done".truecolor(F4, F5, F6)
+                        )
                     }
                 });
                 pb.inc(1);
@@ -154,14 +236,19 @@ fn progress_bar(commands: Vec<Program>, num: u64) -> Result<Arc<MultiProgress>, 
         let _ = h.join();
     }
 
-    pb.finish_with_message(format!("{}", "done".bold().green()));
+    pb.finish_with_message(format!("{}", "done".bold().truecolor(F4, F5, F6)));
 
     // m.clear().unwrap();
 
     println!(
         "{} {}",
-        "::: ALL STARTED IN".bold().green(),
-        HumanDuration(started.elapsed()).to_string().bold().green()
+        "::: ALL UPDATED IN ".bold().truecolor(F4, F5, F6),
+        HumanDuration(started.elapsed())
+            .to_string()
+            .to_uppercase()
+            .bold()
+            .truecolor(F1, F2, F3)
+            .on_truecolor(B1, B2, B3)
     );
 
     Ok(m)
